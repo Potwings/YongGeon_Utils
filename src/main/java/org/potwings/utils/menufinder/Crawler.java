ad15a4b9591938822248622be3aa194e48074bbf
Potwings <ygk0719@gmail.com>
2025-06-15 22:08:24 +0900

    feat: 스크롤하여 1페이지의 음식점 목록 모두 불러온 후 해당 음식점들 메뉴 모두 확인할 수 있도록 기능 개선

--- src/main/java/org/potwings/utils/menufinder/Crawler.java
+++ src/main/java/org/potwings/utils/menufinder/Crawler.java
@@ -3,6 +3,7 @@ package org.potwings.utils.menufinder;
 import java.time.Duration;
 import java.util.List;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -15,7 +16,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Crawler {
 
   private static final String findStore = "강남역 호프";
-  private static final String findMenu = "치킨";
+  private static final String findMenu = "해물고기한판";
 
   public static void main(String[] args) {
     System.setProperty("webdriver.chrome.driver", "C:/zzz/chromedriver-win64/chromedriver.exe");
@@ -38,13 +39,36 @@ public class Crawler {
 
       WebElement searchIframe = wait.until(
           ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#searchIframe")));
-      driver.switchTo().frame(searchIframe);
-      List<WebElement> ouxiqList = driver.findElements(By.className("iHXbN"));
-      wait.until(ExpectedConditions.elementToBeClickable(ouxiqList.get(0))).click();
-      Thread.sleep(500); // 클릭 후 로딩 대기
-      driver.switchTo().defaultContent();
-      processMenuAndMore(driver, wait);  // 메뉴 클릭 및 GHAhO 값 수집
-      driver.switchTo().frame(searchIframe);
+      driver.switchTo().frame(searchIframe); // 검색 결과 iframe으로 이동
+
+      // 스크롤 실행
+      JavascriptExecutor js = (JavascriptExecutor) driver;
+      // 무한스크롤이 적용된 div 선택자 (직접 확인 필요)
+      WebElement scrollDiv = wait.until(ExpectedConditions.visibilityOfElementLocated(
+          By.cssSelector("#_pcmap_list_scroll_container"))); // 해당 페이지의 모든 요소들을 불러오기 위해 스크롤
+
+      long scrollTop = 0;
+      while (true) {
+        // 컨텐츠 로딩 속도를 위해 스크롤 천천히 아래로 이동
+        js.executeScript("arguments[0].scrollTop += arguments[1];", scrollDiv, 700);
+
+        // 약간의 대기 시간 (렌더링 시간 고려)
+        Thread.sleep(1000);
+
+        long newScrollTop = (long) js.executeScript("return arguments[0].scrollTop", scrollDiv);
+        if (scrollTop == newScrollTop) {
+          break;
+        }
+        scrollTop = newScrollTop;
+      }
+      // 음식점 목록 추출
+      List<WebElement> storeList = driver.findElements(By.className("iHXbN"));
+      for (WebElement store : storeList) {
+        wait.until(ExpectedConditions.elementToBeClickable(store)).click();
+        Thread.sleep(3000); // 클릭 후 로딩 대기
+        processMenuAndMore(driver, wait);  // 메뉴 클릭 및 GHAhO 값 수집
+        driver.switchTo().frame(searchIframe); // 검색 iframe으로 이동
+      }
 //      while (true) {
 //        boolean ouxiqProcessed = false;
 //
@@ -90,9 +114,10 @@ public class Crawler {
   // "메뉴" 클릭 및 "더보기" 반복 클릭, GHAhO 수집
   private static void processMenuAndMore(WebDriver driver, WebDriverWait wait) {
     try {
-      WebElement searchIframe = wait.until(
+      driver.switchTo().defaultContent(); //storeIframe을 찾을 수 있도록 전체 화면으로 driver 이동
+      WebElement storeIframe = wait.until(
           ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#entryIframe")));
-      driver.switchTo().frame(searchIframe);
+      driver.switchTo().frame(storeIframe); // 음식점 iframe으로 이동
       List<WebElement> spans = driver.findElements(By.className("veBoZ"));
       for (WebElement span : spans) {
         if (span.getText().equals("메뉴")) {
@@ -101,6 +126,7 @@ public class Crawler {
           break;
         }
       }
+
       WebElement moreMenu = null;
       while (true) {
         try {
@@ -121,17 +147,20 @@ public class Crawler {
       for (WebElement menu : menuList) {
         String menuStr = menu.getText();
         if (menuStr.contains(findMenu)) {
-          if(!haveMenu) {
+          if (!haveMenu) {
             haveMenu = true;
             sb = new StringBuffer();
           }
-          System.out.println("================");
-          sb.append(menuStr);
-          sb.append(",");
-          System.out.println("================");
+          sb.append(menuStr).append(", ");
         }
       }
 
+      if (sb != null) {
+        sb.append("음식점 명 : ").append(driver.findElement(By.className("GHAhO")).getText());
+        System.out.println(sb.toString());
+      }
+
+      driver.switchTo().defaultContent(); // 다음 store 검색을 위해 현재 store Iframe에서 빠져나옴
     } catch (Exception e) {
       System.out.println("메뉴 클릭 처리 중 오류 발생");
       e.printStackTrace();
